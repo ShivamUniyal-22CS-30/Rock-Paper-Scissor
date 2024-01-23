56c6fab9405daccb1d81a58e8b1f1e912506796a
Shivam Uniyal <shivamuniyal1810@gmail.com>
2024-01-23 19:44:29 +0530

    5)Simplified playRound() & tie condition doesn't return undefined

--- script.js
+++ script.js
@@ -11,46 +11,23 @@ function getComputerChoice() {
 // 2 creating function playRound(); 
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        // Accounting for tie by recalling "playRound()"
-        console.log("It's a Tie, Try again ");
-
-        let playerSelection = prompt("enter after tie: ");
-        let computerSelection = getComputerChoice(); 
-
-        console.log(`tie player: ${playerSelection} \ntie comp: ${computerSelection}`);
-        console.log(playRound(playerSelection,computerSelection));
+    if(playerSelection.toLowerCase() === computerSelection){
+        // 3 Accounting for tie by recalling "playRound()"
+        console.log("It's a Tie, Try again "); 
+        return playRound(prompt("Enter after tie: ").toLowerCase(), getComputerChoice());
     }
 
-    // playerSelection == rock
-    else if(playerSelection == "rock" && computerSelection == "scissor"){
-        return "You Win! Rock beats Scissors";
+    // all win condition
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissor") ||
+        (playerSelection == "paper" && computerSelection == "rock") || 
+        (playerSelection == "scissor" && computerSelection == "paper")){ 
+            return 1;
     }
-    else if(playerSelection == "rock" && computerSelection =="paper"){
-        return "You Lose! Paper beats Rock";
-    }
-
-    //payerSelection = paper
-    else if(playerSelection == "paper" && computerSelection =="rock"){
-        return "You Win! Paper beats Rock";
-    }
-    else if(playerSelection == "paper" && computerSelection =="scissor"){
-        return "You Lose! Scissor beats Paper";
-    }
-
-    //playerSelection = scissor
-    else if(playerSelection == "scissor" && computerSelection=="rock"){
-        return "You Lose! Rock beats scissor";
-    }
-    else if(playerSelection == "scissor" && computerSelection=="paper"){
-        return "You Win! Scissor beats Paper"
+    else{ 
+        return 0;
     }
 }
 
 
-let playerSelection = prompt("enter : ");
-let computerSelection = getComputerChoice();
 
-console.log("player: ",playerSelection);
-console.log("computer: ",computerSelection);
-console.log(playRound(playerSelection.toLowerCase(),computerSelection))
